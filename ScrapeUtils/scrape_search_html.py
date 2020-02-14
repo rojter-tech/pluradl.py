f4b3510d7b65eddc74d3c89ba3c76e2fc727f3b2
Daniel Reuter <dreuter@kth.se>
2020-02-14 13:16:35 +0100

    scraping examples

--- ScrapeUtils/scrape_search_html.py
+++ ScrapeUtils/scrape_search_html.py
@@ -1,31 +1,53 @@
 import os, re, json
 
-url=r'https://www.pluralsight.com/courses/'
+# Metadata
+RAW_URL = r'https://www.pluralsight.com/courses/'
+HTML_FILE = os.path.join("data", "search_results.html")
+JSON_OUTPUT_FILE = os.path.join("json_output", "courses.json")
 
-def lookaround_tag(start_tag, end_tag):
+
+def lookaround_tags(start_tag, end_tag):
     lookbehind = r'(?<=' + start_tag + r')'
     lookahead = r'(?=' + end_tag + r')'
     lookaround = re.compile(lookbehind + r'.*?' + lookahead)
-
     return lookaround
 
-search_tag=r'<div class="search-result__title">'
-div_tag=r'</div>'
 
-result_lookaround = lookaround_tag(search_tag, div_tag)
-courseid_lookaround = lookaround_tag(url, '"')
-title_lookaround = lookaround_tag(r'>', r'</a>')
+def store_dic_as_json(filepath, dictionary):
+    path = os.path.dirname(filepath)
+    if not os.path.exists(path):
+        os.mkdir(path)
+    with open(filepath, 'wt') as f:
+        json.dump(dictionary, f, sort_keys=True, indent=4)
 
-courses = {}
-with open("search_results.html", 'rt') as f:
-    for line in f.readlines():
-        search_line = result_lookaround.search(line)
-        if search_line:
-            title_tag = search_line.group()
-            courseid = courseid_lookaround.search(title_tag).group()
-            title = title_lookaround.search(title_tag).group()
-            
-            courses[courseid] = title
 
-with open('courses.json', 'wt') as f:
-    json.dump(courses, f, sort_keys=True, indent=4)
+def scrape_and_store_courses():
+    # Search results encapsulation
+    search_tag=r'<div class="search-result__title">'
+    div_tag=r'</div>'
+    result_lookaround = lookaround_tags(search_tag, div_tag)
+
+    # Encapsulation within search results
+    quote = r'"'
+    gt = r'>'
+    a_tag = r'</a>'
+    courseid_lookaround = lookaround_tags(RAW_URL, quote)
+    title_lookaround = lookaround_tags(gt, a_tag)
+
+    # Parse the content of data/search_results.html and put data in a dicionary
+    courses = {}
+    with open(HTML_FILE, 'rt') as f:
+        for line in f.readlines():
+            search_line = result_lookaround.search(line)
+            if search_line:
+                title_tag = search_line.group()
+                courseid = courseid_lookaround.search(title_tag).group()
+                title = title_lookaround.search(title_tag).group()
+                courses[courseid] = title
+    
+    # Store dictionary as json
+    store_dic_as_json(JSON_OUTPUT_FILE, courses)
+
+
+if __name__ == "__main__":
+    scrape_and_store_courses()
