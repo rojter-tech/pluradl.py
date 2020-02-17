35021f3cb1d07b4699d370c34583b57050e3aaf8
Daniel Reuter <dreuter@kth.se>
2020-02-17 13:51:55 +0100

    more scrape examples

---
+++ ScrapeUtils/01_filter_results_to_txt.py
@@ -0,0 +1,16 @@
+import json, os, re
+JSON_FILE = os.path.join("json_output", "courses.json")
+
+with open(JSON_FILE, 'rt') as f:
+    json_load = json.load(f)
+
+course_ids = list(json_load.keys())
+
+search_string = r'kali-linux'
+results_path = os.path.join("filtered_results", search_string + r'.txt')
+
+with open(results_path, 'wt') as f:
+    for course_id in course_ids:
+        search = re.search(search_string, course_id)
+        if search:
+            f.write(course_id + '\r\n')
