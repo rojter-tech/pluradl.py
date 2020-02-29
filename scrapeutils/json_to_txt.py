733b5ebbb9a3f949fd10e9b8f39bc5b030629863
Daniel Reuter <dreuter@kth.se>
2020-02-29 16:51:32 +0100

    update courses

--- scrapeutils/json_to_txt.py
+++ scrapeutils/json_to_txt.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 import json, os, re
 JSON_FILE = os.path.join("data", "courses.json")
+mode = 'search'
+mode = 'all'
 
 with open(JSON_FILE, 'rt') as f:
     json_load = json.load(f)
@@ -8,10 +10,22 @@ with open(JSON_FILE, 'rt') as f:
 course_ids = list(json_load.keys())
 
 search_string = r'kali-linux'
-results_path = os.path.join("filtered_results", search_string + r'.txt')
 
-with open(results_path, 'wt') as f:
-    for course_id in course_ids:
-        search = re.search(search_string, course_id)
-        if search:
-            f.write(course_id + '\r\n')
+if mode == 'search':
+    results_path = os.path.join("filtered_results", search_string + r'.txt')
+elif mode == 'all':
+    results_path = os.path.join("filtered_results", r"all_courses" + r'.txt')
+else:
+    results_path = None
+
+if results_path:
+    with open(results_path, 'wt') as f:
+        for course_id in course_ids:
+            if mode == 'search':
+                search = re.search(search_string, course_id)
+                if search:
+                    f.write(course_id + '\r\n')
+            elif mode == 'all':
+                f.write(course_id + '\r\n')
+            else:
+                break
