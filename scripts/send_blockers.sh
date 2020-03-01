e3c685fde913a5bb3b7ad0e490994e53f65d22a3
Daniel Reuter <dreuter@kth.se>
2020-03-01 04:39:24 +0100

    update metadata

--- scripts/send_blockers.sh
+++ scripts/send_blockers.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 clear 
-tar -cvf ../exercise_files/ghost.tar ../exercise_files/*.zip
+tar -cvf ghost.tar ../exercise_files/*.zip
 rm /run/user/1000/gvfs/smb-share\:server\=192.168.10.10\,share\=download/ghost.tar
-cp ../exercise_files/ghost.tar /run/user/1000/gvfs/smb-share:server=192.168.10.10,share=download/
-rm ../exercise_files/ghost.tar
+cp ghost.tar /run/user/1000/gvfs/smb-share:server=192.168.10.10,share=download/
 clear
