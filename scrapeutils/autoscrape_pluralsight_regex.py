5b09de394f2e086b9e594ba7ec260e423a70dee7
Daniel Reuter <dreuter@kth.se>
2020-03-02 21:28:04 +0100

    reshaping source

--- scrapeutils/autoscrape_pluralsight_regex.py
+++ scrapeutils/autoscrape_pluralsight_regex.py
@@ -1,17 +1,13 @@
-from __future__ import unicode_literals
-import os, json, re, datetime
-from selenium import webdriver
-if not os.path.exists('data'):
-    os.mkdir('data')
+from utils import *
 
-SCRAPE_URL = r'https://www.pluralsight.com/search?categories=course&sort=title'
-HTML_OUTPUT_FILE = os.path.join("...", "data", "search_results.html")
-JSON_OUTPUT_FILE = os.path.join("...", "data", "courses.json")
+SCRIPTPATH=os.path.dirname(os.path.abspath(sys.argv[0]))
+JSON_OUTPUT_FILE = os.path.abspath(os.path.join(SCRIPTPATH, '..', "data", "courses.json"))
+SEARCH_URL = r'https://www.pluralsight.com/search?categories=course&sort=title'
 
 
-def load_and_save_html(SCRAPE_URL, HTML_OUTPUT_FILE):
+def load_and_save_html(SEARCH_URL, HTML_OUTPUT_FILE):
     driver = webdriver.Firefox()
-    driver.get(SCRAPE_URL)
+    driver.get(SEARCH_URL)
     load_more_results = r'jQuery(".button--secondary").click()'
 
     for i in range(50000):
@@ -23,105 +19,30 @@ def load_and_save_html(SCRAPE_URL, HTML_OUTPUT_FILE):
     driver.close()
 
 
-def outer_search_html(HTML_FILE, class_name):
+def outer_search_html(source_html, class_name):
     read_state=False; track=0; search_snippets=[]
-    with open(HTML_FILE, 'rt') as f:
-        for line in f.readlines():
-            if re.search(r'class=' + r'"' + class_name + r'"', line):
-                read_state = True
-                search_result = []
-            if read_state:
-                search_result.append(line)
-                n_open = len(re.findall(r'<div', line))
-                n_close = len(re.findall(r'/div>', line))
-                track+=n_open;   track-=n_close
-                if track == 0:
-                    read_state = False
-                    search_snippets.append(''.join(search_result))
-    return search_snippets
-
-
-def outer_search_snippet(snippet, class_name):
-    read_state=False; track=0; search_result = None
-    for line in snippet.split('\n'):
+    for line in source_html.split('\n'):
         if re.search(r'class=' + r'"' + class_name + r'"', line):
             read_state = True
             search_result = []
         if read_state:
-            search_result.append(line.strip())
+            search_result.append(line)
             n_open = len(re.findall(r'<div', line))
             n_close = len(re.findall(r'/div>', line))
             track+=n_open;   track-=n_close
             if track == 0:
                 read_state = False
-    return search_result
-
-
-def lookaround_tags(start_tag, end_tag):
-    # Contruct regular expression
-    lookbehind = r'(?<=' + start_tag + r')'
-    lookahead = r'(?=' + end_tag + r')'
-    wildcard = r'.*?'
-    regex = "%s%s%s"%(lookbehind,wildcard,lookahead)
-
-    # Compile it and return
-    lookaround = re.compile(regex)
-    return lookaround
-
-
-def store_dict_as_json(dictionary, filepath):
-    path = os.path.dirname(filepath)
-    if not os.path.exists(path):
-        os.mkdir(path)
-    with open(filepath, 'wt') as f:
-        json.dump(dictionary, f, sort_keys=True, indent=4)
-
-
-def return_lookaround_text(lookaround_search):
-    if lookaround_search:
-        lookaround_text = lookaround_search.group()
-        return lookaround_text
-
-
-def return_rating(rating_snippet):
-    rating = 'none'
-    if rating_snippet:
-        for row in rating_snippet:
-            number = re.search(r'[0-9]{1,}', row)
-            if number:
-                rating = number.group()
-    if rating != 'none':
-        return int(rating)
-    else:
-        return 'none'
-
-
-def return_length(length_text):
-    hours=None; minutes=None;
-    if length_text:
-        hours_search = re.search(r'[0-9]{1,}(?=h)', length_text)
-        minutes_search = re.search(r'[0-9]{1,}(?=m)', length_text)
-        if hours_search:
-            hours = int(hours_search.group())
-        if minutes_search:
-            minutes = int(minutes_search.group())
-    
-    if hours and minutes:
-        time = hours*60 + minutes
-    elif hours:
-        time = hours*60
-    elif minutes:
-        time = minutes
-    else:
-        time = 'none'
-    return time
+                search_snippets.append(''.join(search_result))
+    return search_snippets
 
 
 def main():
-    load_and_save_html(SCRAPE_URL, HTML_OUTPUT_FILE)
-    class_name = r'search-result columns'
-    search_snippets = outer_search_html(HTML_OUTPUT_FILE, class_name)
+    course_dict = load_stored_json(JSON_OUTPUT_FILE)
+    source_data = get_courselist_source(SEARCH_URL, n_pages=1)
 
+    class_name = r'search-result columns'
+    search_snippets = outer_search_html(source_data, class_name)
+    
     title_tag=r'<div class="search-result__title">'
     author_tag=r'<div class="search-result__author">'
     level_tag=r'<div class="search-result__level">'
@@ -138,7 +59,6 @@ def main():
     gt_a_lookaround = lookaround_tags(gt, a_tag)
 
 
-    courses = {}
     for html_input in search_snippets:
         thiscourse = {}
         title_outer_text = return_lookaround_text(title_lookaround.search(html_input))
@@ -150,21 +70,24 @@ def main():
             level_text = return_lookaround_text(level_lookaround.search(html_input))
             date_text = return_lookaround_text(date_lookaround.search(html_input))
             length_text = return_lookaround_text(length_lookaround.search(html_input))
-            lenght = return_length(length_text)
+            lenght = get_length(length_text)
             rating_snippet = outer_search_snippet(html_input, r'search-result__rating')
             rating = return_rating(rating_snippet)
 
             if author_text and not '{' in author_text:
-                thiscourse['url'] = url_text
-                thiscourse['title'] = name_text
-                thiscourse['author'] = author_text.split('by ')[-1]
-                thiscourse['level'] = level_text
-                thiscourse['date'] = date_text
+                thiscourse['url'] = url_text.strip()
+                thiscourse['title'] = name_text.strip()
+                thiscourse['author'] = author_text.strip().split('by ')[-1]
+                thiscourse['level'] = level_text.strip()
+                thiscourse['date'] = date_text.strip()
                 thiscourse['length'] = lenght
                 thiscourse['rating'] = rating
-                courses[course_id] = thiscourse
+                course_dict[course_id] = thiscourse
     
-    store_dict_as_json(courses, JSON_OUTPUT_FILE)
+    print('')
+    print('Loaded', len(course_dict), 'courses.', 'Saving results to', JSON_OUTPUT_FILE)
+    store_dict_as_json(course_dict, JSON_OUTPUT_FILE)
+    print('Done.')
 
 
 if __name__ == "__main__":
--- scrapeutils/autoscrape_pluralsight_soup.py
+++ scrapeutils/autoscrape_pluralsight_soup.py
@@ -1,152 +1,14 @@
-from __future__ import unicode_literals
-import os, json, re, sys, codecs, sys
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-from bs4 import BeautifulSoup
-if os.name == 'nt':
-    clear = lambda: os.system('cls')
-elif os.name == 'posix':
-    clear = lambda: os.system('clear')
-else:
-    clear = lambda: None
+from utils import *
 
 SCRIPTPATH=os.path.dirname(os.path.abspath(sys.argv[0]))
-SEARCH_URL = r'https://www.pluralsight.com/search?categories=course&sort=title'
 JSON_OUTPUT_FILE = os.path.abspath(os.path.join(SCRIPTPATH, '..', "data", "courses.json"))
-LOAD_MORE_RESULTS = r'jQuery(".button--secondary").click()'
-RESULT_BUTTON=r'//*[@id="search-results-section-load-more"]'
-
-
-def set_driver():
-    firefox_options = Options()
-    firefox_options.add_argument("--headless")
-    firefox_options.add_argument("--window-size=640x360")
-    firefox_options.add_argument("--disable-notifications")
-    firefox_options.add_argument('--no-sandbox')
-    firefox_options.add_argument('--disable-gpu')
-    firefox_options.add_argument('--disable-software-rasterizer')
-    driver = webdriver.Firefox(options=firefox_options)
-    return driver
-
-
-def wait_for_access(driver, XPATH, timer=10):
-    element = WebDriverWait(driver, timer).until(
-    EC.element_to_be_clickable((By.XPATH, XPATH)))
-    return element
-
-
-def get_source(n_pages=500):
-    driver = set_driver()
-    driver.get(SEARCH_URL)
-
-    for i in range(n_pages):
-        try:
-            wait_for_access(driver, RESULT_BUTTON)
-            if i > 0:
-                clear()
-                print("Loading more results " + i*'.')
-            driver.execute_script(LOAD_MORE_RESULTS)
-            print("Loaded 25 courses from resultpage","{:03d}".format(i+1), "of ~385.", end=' ')
-        except TimeoutException:
-            print('No more results could be found. Preparing data ...')
-            break
-    print("Finalizing reading from source.")
-    for i in range(5000):
-        driver.execute_script(LOAD_MORE_RESULTS)
-    print('')
-    output_html = driver.page_source
-    driver.close()
-    return output_html
-
-
-def get_course_elements(course):
-    title = course.find("div", class_="search-result__title")
-    author = course.find("div", class_="search-result__author")
-    level = course.find("div", class_="search-result__level")
-    date = course.find("div", class_="search-result__date")
-    length = course.find("div", class_="search-result__length")
-    rating = course.find("div", class_="search-result__rating")
-    return title, author, level, date, length, rating
-
-
-def get_length(length_element):
-    length_text = length_element.get_text()
-    hours=None; minutes=None
-    if length_text:
-        hours_search = re.search(r'[0-9]+(?=h)', length_text)
-        minutes_search = re.search(r'[0-9]+(?=m)', length_text)
-        if hours_search:
-            hours = int(hours_search.group())
-        if minutes_search:
-            minutes = int(minutes_search.group())
-    if hours and minutes:
-        time = hours*60 + minutes
-    elif hours:
-        time = hours*60
-    elif minutes:
-        time = minutes
-    else:
-        time = 'none'
-    return time
-
-
-def get_rating(rating_elem):
-    if rating_elem:
-        rating = re.search(r'[0-9]+', rating_elem.get_text())
-        if rating:
-            rating = int(rating.group())
-        else:
-            rating = ""
-    else:
-        rating = ""
-    return rating
-
-
-def get_course_elements_texts(course_elements):
-    title = course_elements[0].get_text()
-    url = course_elements[0].find('a').get('href')
-    courseid = url.split('/')[-1]
-    author = course_elements[1].get_text()
-    level = course_elements[2].get_text()
-    date = course_elements[3].get_text()
-    length = get_length(course_elements[4])
-    rating = get_rating(course_elements[5])
-    return courseid, url, title, author, level, date, length, rating
-
-
-def get_course_dictionary(course_texts):
-    thiscourse = {}
-    thiscourse["url"] = course_texts[1].strip()
-    thiscourse["title"] = course_texts[2].strip()
-    thiscourse["author"] = course_texts[3].strip().split('by ')[-1]
-    thiscourse["level"] = course_texts[4].strip()
-    thiscourse["date"] = course_texts[5].strip()
-    thiscourse["length"] = course_texts[6]
-    thiscourse["rating"] = course_texts[7]
-    return thiscourse
-
-
-def store_dict_as_json(dictionary, filepath):
-    path = os.path.dirname(filepath)
-    if not os.path.exists(path):
-        os.mkdir(path)
-    with codecs.open(filepath, 'w', "utf-8") as f:
-        json_string = json.dumps(dictionary, sort_keys=True, indent=4, ensure_ascii=False)
-        f.write(json_string)
+SEARCH_URL = r'https://www.pluralsight.com/search?categories=course&sort=title'
 
 
 def main():
-    if os.path.exists(JSON_OUTPUT_FILE):
-        with codecs.open(JSON_OUTPUT_FILE, 'r', 'utf-8') as json_file:
-            courses = json.load(json_file)
-    else:
-        courses = {}
+    course_dict = load_stored_json(JSON_OUTPUT_FILE)
     print("Loading web driver ...")
-    source_data = get_source(n_pages=400)
+    source_data = get_courselist_source(SEARCH_URL, n_pages=1)
     soup = BeautifulSoup(source_data, 'html.parser')
     course_results = soup.find_all("div", class_="search-result__info")
     i=0
@@ -159,10 +21,10 @@ def main():
         i+=1
         course_elements = get_course_elements(this_course)
         course_texts = get_course_elements_texts(course_elements)
-        courses[course_texts[0]] = get_course_dictionary(course_texts)
+        course_dict[course_texts[0]] = get_course_dictionary(course_texts)
     print('')
-    print('Loaded', len(courses), 'courses.', 'Saving results to', JSON_OUTPUT_FILE)
-    store_dict_as_json(courses, JSON_OUTPUT_FILE)
+    print('Loaded', len(course_dict), 'courses.', 'Saving results to', JSON_OUTPUT_FILE)
+    store_dict_as_json(course_dict, JSON_OUTPUT_FILE)
     print('Done.')
 
 
