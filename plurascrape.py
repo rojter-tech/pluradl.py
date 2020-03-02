cc46b903d6eb47ba8f8e27fe2f593cc2aba98409
Daniel Reuter <dreuter@kth.se>
2020-03-02 22:27:31 +0100

    path referencewq

--- plurascrape.py
+++ plurascrape.py
@@ -1,14 +1,15 @@
 from plura_dl.scrapeutils import *
 
 SCRIPTPATH=os.path.dirname(os.path.abspath(sys.argv[0]))
-JSON_OUTPUT_FILE = os.path.abspath(os.path.join("data", "courses.json"))
+JSON_OUTPUT_FILE = os.path.abspath(os.path.join(SCRIPTPATH, "data", "courses.json"))
 SEARCH_URL = r'https://www.pluralsight.com/search?categories=course&sort=title'
 
+print(SCRIPTPATH)
 
 def main():
     course_dict = load_stored_json(JSON_OUTPUT_FILE)
     print("Loading web driver ...")
-    source_data = get_courselist_source(SEARCH_URL, n_pages=400, finish_rounds=100)
+    source_data = get_courselist_source(SEARCH_URL, n_pages=10, finish_rounds=10)
     soup = BeautifulSoup(source_data, 'html.parser')
     course_results = soup.find_all("div", class_="search-result__info")
     i=0
