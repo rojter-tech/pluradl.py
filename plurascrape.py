ce184f0e12defd513d5c1c9be4500789b7e1a6e7
Daniel Reuter <dreuter@kth.se>
2020-05-10 04:17:29 +0200

    scraping course description

--- plurascrape.py
+++ plurascrape.py
@@ -4,9 +4,6 @@ from plura_dl.scrapeutils import (
     out,
     time,
     load_stored_json,
-
-
-
     get_course_dictionary,
     store_dict_as_json
 )
@@ -17,12 +14,12 @@ from plura_dl.dependentutils import (
     get_course_elements_texts,
 )
 
-SCRIPTPATH=os.path.dirname(os.path.abspath(sys.argv[0]))
-JSON_OUTPUT_FILE = os.path.abspath(os.path.join(SCRIPTPATH, "data", "courses.json"))
-SEARCH_URL = r'https://www.pluralsight.com/search?categories=course&sort=title'
-
 
 def main():
+    SCRIPTPATH=os.path.dirname(os.path.abspath(sys.argv[0]))
+    JSON_OUTPUT_FILE = os.path.abspath(os.path.join(SCRIPTPATH, "data", "courses.json"))
+    SEARCH_URL = r'https://www.pluralsight.com/search?categories=course&sort=title'
+
     start = time()
     course_dict = load_stored_json(JSON_OUTPUT_FILE)
     source_data = get_courselist_source(SEARCH_URL, n_pages=400, finish_rounds=100)
@@ -32,6 +29,7 @@ def main():
     out.write("Done.\n"); out.flush()
     i=0
     out.write("Processing course metadata ."); out.flush()
+    
     for this_course in course_results:
         if i%400 == 0:
             out.write('.'); out.flush()
@@ -39,6 +37,7 @@ def main():
         course_elements = get_course_elements(this_course)
         course_texts = get_course_elements_texts(course_elements)
         course_dict[course_texts[0]] = get_course_dictionary(course_texts)
+    
     msg = ' Loaded ' + str(len(course_dict)) + ' courses.' + '\nSaving results to ' + JSON_OUTPUT_FILE
     out.write(msg); out.flush()
     store_dict_as_json(course_dict, JSON_OUTPUT_FILE)
---
+++ plurascrape_post.py
@@ -0,0 +1,34 @@
+from plura_dl.scrapeutils import (
+    store_dict_as_json
+)
+
+import json, os, re, requests, codecs, time
+
+def main():
+    SCRIPTPATH = os.path.dirname(os.path.abspath(__file__))
+    json_in = os.path.join(SCRIPTPATH, "data", "courses.json")
+    JSON_FILE = os.path.abspath(json_in)
+    JSON_OUTPUT = os.path.join(SCRIPTPATH, "data", "courses_d.json")
+
+    with open(JSON_FILE, 'rt') as f:
+        json_load = json.load(f)
+
+    course_ids = list(json_load.keys())
+
+    start = time.time(); i=0
+    for course_id in course_ids:
+        url = json_load[course_id]["url"]
+        r = requests.get(url)
+        test = re.search(r'<p>.*</p>', r.text)
+        get_description = test.group()
+        description = get_description[3:-4]
+        json_load[course_id]["description"] = description
+        i+=1
+        if i%100 == 1:
+            print("Iteration:", i, "performed in:", time.time() - start, "seconds.")
+        time.sleep(2.5)
+    
+    store_dict_as_json(json_load, JSON_OUTPUT)
+
+if __name__ == "__main__":
+    main()
